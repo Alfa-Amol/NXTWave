104eb7d266cabffd97d13e73a0555b34ecfd13c2
Alfa-Amol <bandalamol836@gmail.com>
2025-04-11 20:41:30 +0530

    1st commit

---
+++ src/Componants/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css';
+
+
+
+function Navbar() {
+  return (
+    
+    <div className='Navbar '>
+        <h3>SHOP</h3>
+        <h3>SKILLS</h3>
+         <h3>STORIES</h3>
+        <h3>ABOUT</h3>
+        <h3>CONTACT US</h3>
+    </div>
+  )
+}
+
+export default Navbar
